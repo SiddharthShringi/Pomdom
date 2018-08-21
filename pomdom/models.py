18c5bb3031e8781fcf4d788fa30aa3ecfeeed08b
BlackSamsung <siddharthshringi@gmail.com>
2018-08-21 10:45:00 +0530

    write model and register that model in admin file

--- pomdom/models.py
+++ pomdom/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_date = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Task(BaseModel):
+    Priority_Choices = (
+        ('H', 'High'),
+        ('M', 'Medium'),
+        ('L', 'Low'),
+    )
+
+    Status_Choices = (
+        ('D', 'Done'),
+        ('P', 'Pending')
+    )
+    name = models.CharField(max_length=30)
+    priority = models.CharField(max_length=1, choices=Priority_Choices, default='M')
+    status = models.CharField(max_length=1, choices=Status_Choices, default='P')
+
+    def __str__(self):
+        return self.name
